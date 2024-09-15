e4179ef0a1a24ae9247dbb32765e0ffdb2ca133b
MarcoTecsup159 <168231703+MarcoTecsup159@users.noreply.github.com>
2024-09-15 10:48:27 -0500

    Agregar funciones Scaffold

---
+++ app/src/main/java/com/example/lab06/MainActivity.kt
@@ -0,0 +1,156 @@
+package com.example.lab06
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lab06.ui.theme.Lab06Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CustomScaffold()
+        }
+    }
+}
+
+@Composable
+fun CustomScaffold() {
+    Scaffold(
+        topBar = { CustomTopBar() },
+        bottomBar = { CustomBottomBar() },
+        floatingActionButton = { CustomFAB() },
+        content = { padding ->
+            CustomContent(padding)
+        }
+    )
+}
+
+@Composable
+fun CustomContent(padding: PaddingValues) {
+    Column(
+        // Modificadores de estilo de la columna
+        modifier = Modifier
+            // Ocupar todo el espacio disponible
+            .fillMaxSize()
+            .padding(padding),
+
+        // Contenido de la aplicación
+        content = {
+            Text(text = "My app content")
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTopBar() {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Rounded.Menu, contentDescription = null)
+            }
+        },
+        title = { Text(text = "Sample Title") },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Rounded.Search,
+                    contentDescription = null
+                )
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Outlined.AccountCircle,
+                    contentDescription = null
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun CustomBottomBar() {
+    BottomAppBar {
+        IconButton(onClick = { print("Build") }) {
+            Icon(Icons.Filled.Build, contentDescription = "Build description")
+        }
+        IconButton(onClick = { print("Menu") }) {
+            Icon(
+                Icons.Filled.Menu,
+                contentDescription = "Menu description",
+            )
+        }
+    }
+}
+
+
+@Composable
+fun CustomFAB() {
+    FloatingActionButton(
+        // Color de fondo
+        //backgroundColor = MaterialTheme.colors.primary,
+        // Acción al hacer clic en el botón (sin definir)
+        onClick = { /*TODO*/ }) {
+        Text(
+            fontSize = 24.sp, // Tamaño de fuente del texto del botón
+            text = "+" // Texto del botón
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCustomScaffold() {
+    CustomScaffold() // Para mostrar todo en una sola vista de preview
+}
