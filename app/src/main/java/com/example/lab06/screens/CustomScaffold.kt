331bdb385f9fe76fbaf63b5f1258d0f3cb0da588
MarcoTecsup159 <168231703+MarcoTecsup159@users.noreply.github.com>
2024-09-15 19:36:40 -0500

    Agregar funcionalidad al boton flotante

--- app/src/main/java/com/example/lab06/screens/CustomScaffold.kt
+++ app/src/main/java/com/example/lab06/screens/CustomScaffold.kt
@@ -21,30 +21,38 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.lab06.navigation.AppScreens
 
 @Composable
 fun CustomScaffold(navController: NavController) {
+    var count by remember { mutableStateOf(0) }
+
     Scaffold(
         topBar = { CustomTopBar(navController) },
         bottomBar = { CustomBottomBar(navController) },
-        floatingActionButton = { CustomFAB() },
+        floatingActionButton = { CustomFAB(onClick = { count++ }) },
         content = { padding ->
-            CustomContent(padding)
+            CustomContent(padding, count)
         }
     )
 }
 
 @Composable
-fun CustomContent(padding: PaddingValues) {
+fun CustomContent(padding: PaddingValues, count: Int) {
     Column(
         // Modificadores de estilo de la columna
         modifier = Modifier
@@ -54,7 +62,7 @@ fun CustomContent(padding: PaddingValues) {
 
         // Contenido de la aplicación
         content = {
-            Text(text = "My app content")
+            Text(text = "Se ha apretado el boton $count veces", style = MaterialTheme.typography.titleLarge)
         }
     )
 }
@@ -114,15 +122,14 @@ fun CustomBottomBar(navController: NavController) {
 
 
 @Composable
-fun CustomFAB() {
+fun CustomFAB(onClick: () -> Unit) {
     FloatingActionButton(
-        // Color de fondo
-        //backgroundColor = MaterialTheme.colors.primary,
-        // Acción al hacer clic en el botón (sin definir)
-        onClick = { /*TODO*/ }) {
+        containerColor = MaterialTheme.colorScheme.primary,
+        onClick = onClick
+    ) {
         Text(
-            fontSize = 24.sp, // Tamaño de fuente del texto del botón
-            text = "+" // Texto del botón
+            fontSize = 24.sp,
+            text = "+"
         )
     }
 }
